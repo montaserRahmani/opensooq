4c717b5fb6931ecf6ec521fb22347bb769ced6d9
montaserRahmani <montaser.rah@gmail.com>
2017-04-12 21:47:31 +0300

    added console command

--- console/controllers/removeOldPosts.php
+++ console/controllers/removeOldPosts.php
@@ -7,7 +7,8 @@ use app\frontend\models\Post;
 
 class RemoveOldPostsController extends Controller
 {
-    
+    //i didnt add created_at field in the post table so i cant delete old posts, its easy to do
+    //but this to demonstrate the console command only
     public function actionRemove()
     {   
         Post::delete()->where('id = 1');
